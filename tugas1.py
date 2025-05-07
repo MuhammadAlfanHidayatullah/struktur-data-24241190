a347fa99b40b1ae5e9dd04f63cdba82c2a78c61d
I love my self❤️💙 <anjayalfan@gmail.com>
2025-05-07 23:01:46 +0800

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,41 @@
+#perbandingan kemungkinan nilai = true/salah (bolean)
+
+#nim:24241190
+
+#hasil <
+a = 24
+b = 90
+hasil = a < b
+print(hasil)
+
+#hasil >
+a = 24
+b = 90
+hasil = a > b
+print(hasil)
+
+#hasil <=
+a = 24
+b = 90
+hasil = a <= b
+print(hasil)
+
+#hasil >=
+a = 24
+b = 90
+hasil = a >= b
+print(hasil)
+
+#hasil !=
+a = 24
+b = 90
+hasil = a != b
+print(hasil)
+
+#hasil not
+a = 24
+b = 90
+if not a != b:
+    print("Nilai a sama dengn nlai b")
+else:
+    print("Nilai a tidak sama nilai b")
